7ae24f719f34b00b46812452268bb433191c270d
kalanto <sandrewbooth@gmail.com>
2016-05-19 23:03:13 -0600

    made scores an array

--- server/schema/scoreSchema.js
+++ server/schema/scoreSchema.js
@@ -2,22 +2,6 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var scoreSchema = new Schema ({
-      1: {type: Number}
-   ,  2: {type: Number}
-   ,  3: {type: Number}
-   ,  4: {type: Number}
-   ,  5: {type: Number}
-   ,  6: {type: Number}
-   ,  8: {type: Number}
-   ,  9: {type: Number}
-   ,  10: {type: Number}
-   ,  11: {type: Number}
-   ,  12: {type: Number}
-   ,  13: {type: Number}
-   ,  14: {type: Number}
-   ,  15: {type: Number}
-   ,  16: {type: Number}
-   ,  17: {type: Number}
-   ,  18: {type: Number}
+      score: [{type:Number}]
 })
 module.exports = mongoose.model('score', scoreSchema)
