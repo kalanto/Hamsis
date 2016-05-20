9d050420871551b6fa51a3753384a731ff5b0559
kalanto <sandrewbooth@gmail.com>
2016-05-20 14:41:58 -0600

    first draft

--- server/schema/playerSchema.js
+++ server/schema/playerSchema.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var playerSchema = new Schema ({
-      name: {
-         first: {type:String,required:true}
-      ,  last:{type:String,required:true}
-   }
-   ,  email: {type:String,required:true}
-   ,  score:{type:Schema.ObjectId, ref:'score'}
-   ,  course:{type:Schema.ObjectId, ref:'course'}
-      })
+   name: {
+      first: {type:String,required:true}
+   ,  last:{type:String,required:true}
+}
+,  email: {type:String,required:true}
+,  score:{type:Schema.ObjectId, ref:'score'}
+,  course:{type:Schema.ObjectId, ref:'course'}
+   })
 module.exports = mongoose.model('player', playerSchema)
