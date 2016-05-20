56eba540fd1fa8d0259068b42218b9d8e908a0c9
kalanto <sandrewbooth@gmail.com>
2016-05-19 22:13:29 -0600

    live scoring fixes

--- public/JS/controller.js
+++ public/JS/controller.js
@@ -1,7 +1,7 @@
 angular.module('app').controller('mainController', function($scope, mainService){
 
+//takes in live scores and loops through the adding them as the player enters them on the front end
 $scope.score = []
-
 $scope.updateScore = function(score){
   $scope.total = mainService.totalScore(score);
 }
--- public/JS/service.js
+++ public/JS/service.js
@@ -1,5 +1,5 @@
 angular.module('app').service('mainService', function(){
-
+//actual function called by controller to loop through the scores array and total playerScore
    this.totalScore = function(score){
      var playerScore = 0;
       for (var i = 0; i < score.length; i++) {
