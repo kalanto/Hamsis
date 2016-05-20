2b8c29227609f9652542f1293d46d3974ec6faa5
kalanto <sandrewbooth@gmail.com>
2016-05-20 11:10:06 -0600

    first edition

--- server/schema/scoreSchema.js
+++ server/schema/scoreSchema.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var scoreSchema = new Schema ({
-      score: [{type:Number}]
+      score: {type:Array,required:true}
 })
 module.exports = mongoose.model('score', scoreSchema)
