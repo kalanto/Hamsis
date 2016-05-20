8583f7f34bf96181004b2715be2d1fa18afc1373
kalanto <sandrewbooth@gmail.com>
2016-05-20 11:46:58 -0600

    frist draft

--- server/schema/playerSchema.js
+++ server/schema/playerSchema.js
@@ -5,9 +5,9 @@ var playerSchema = new Schema ({
       name: {
          first: {type:String,required:true}
       ,  last:{type:String,required:true}
-   ,  email: {type:Email,required:true}
-   ,  score:{type:Schema.ObjectID, ref:'score'}
-   ,  course:{tyep:Schema.ObjectID, ref:'course'}
-      }
-})
-module.exports = mongoose.model('score', scoreSchema)
+   }
+   ,  email: {type:String,required:true}
+   ,  score:{type:Schema.ObjectId, ref:'score'}
+   ,  course:{type:Schema.ObjectId, ref:'course'}
+      })
+module.exports = mongoose.model('player', playerSchema)
--- server/schema/scoreSchema.js
+++ server/schema/scoreSchema.js
@@ -4,4 +4,4 @@ var Schema = mongoose.Schema
 var scoreSchema = new Schema ({
       score: {type:Array,required:true}
 })
-module.exports = mongoose.model('score', scoreSchema)
+module.exports = mongoose.model('score',scoreSchema)
