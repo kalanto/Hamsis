89f7fc70d1d7d8fd4caa5452b8610b46afa6cbf2
kalanto <sandrewbooth@gmail.com>
2016-05-20 14:40:13 -0600

    first draft

--- server/endpoints/playerEP.js
+++ server/endpoints/playerEP.js
@@ -1,61 +1,61 @@
 var Player = require('../../server/schema/playerSchema.js')
 module.exports={
 //create new player
-      playerPost: function(req,res,next){
-         console.log(req.body)
-            var player = new Score(req.body)
-               player.save(function(e,r){
-                  console.log(e)
-                  if(e){
-                     res.status(500).send(e)
-                  }
-                  else{
-                     res.send(r)
-                  }
-               })
-      }
+playerCreate: function(req,res,next){
+   console.log(req.body)
+      var player = new Player(req.body)
+         player.save(function(e,r){
+            console.log(e)
+               if(e){
+                res.status(500).send(e)
+               }
+               else{
+                res.send(r)
+               }
+            })
+}
 //get player request
-   ,   player: function(req,res){
-         Score.find(req.query, function(e, r){
-            if(e){
-               res.status(500).json(e)
-            }
-            else{
-               res.json(r)
-            }
+,   players: function(req,res){
+      Score.find(req.query, function(e, r){
+         if(e){
+            res.status(500).json(e)
+         }
+         else{
+            res.json(r)
+         }
          })
-      }
+}
 //get player by ID request
-   ,  playerID: function(req,res){
-      Score.findOneAndUpdate(req.params.ID, function(e,r){
-         if(e){
-            res.status(500).json(e)
-         }
-         else{
-            res.send(r)
-         }
-      })
+,  playerID: function(req,res){
+Score.findOneAndUpdate(req.params.ID, function(e,r){
+   if(e){
+      res.status(500).json(e)
    }
+   else{
+      res.send(r)
+   }
+})
+}
 //update player by ID request
-   ,  playerUpdate: function(req,res){
-         Score.findOneAndUpdate(req.params.ID, function(e,r){
-            if(e){
-               res.status(500).json(e)
-            }
-            else{
-               res.send(r.data)
-            }
-         })
+,  playerUpdate: function(req,res){
+   Score.findOneAndUpdate(req.params.ID, function(e,r){
+      if(e){
+         res.status(500).json(e)
       }
+      else{
+         res.send(r.data)
+      }
+   })
+}
 //delete player by ID request
-   ,  playerDelete: function(req,res){
-      Score.findOneAndRemove(req.params.ID, function(e,r){
-         if(e){
-            res.status(500).json(e)
-         }
-         else{
-            return res.send(r)
-         }
-      })
-   }
-   }
+,  playerDelete: function(req,res){
+   Score.findOneAndRemove(req.params.ID, function(e,r){
+      if(e){
+         res.status(500).json(e)
+      }
+      else{
+         return res.send(r)
+      }
+   })
+}
+}
