d70e514f7886315bf41c39622904b96b502d36ea
kalanto <sandrewbooth@gmail.com>
2016-05-21 19:56:27 -0600

    added yellow tees and course name

--- server/schema/courseSchema.js
+++ server/schema/courseSchema.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var courseSchema = new Schema ({
-   teeYardage:[{   proTee: [{type: Number,required: true}]
+   name:{type:String,require:true}
+,  teeYardage:[{proTee: [{type: Number,required: true}]
    ,  blueTee: [{type:Number, required: true}]
    ,  whiteTee: [{type:Number, required: true}]
+   ,  yellowTee: [{type:Number,requiere:true}]
    , redTee: [{type:Number, required:true}]
    }]
 ,  par:[{
