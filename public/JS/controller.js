daa46636d9fce45e0ad5da9393cd380f6aa2db32
kalanto <sandrewbooth@gmail.com>
2016-05-21 19:53:47 -0600

    finished course creator

--- public/JS/controller.js
+++ public/JS/controller.js
@@ -1,8 +1,29 @@
 angular.module('app').controller('mainController', function($scope, mainService){
-
+///////////////////////////////////////////////////////////////
 //takes in live scores and loops through the adding them as the player enters them on the front end
+//////////////////////////////////////////////////////////////
+$scope.test = "right?";
 $scope.score = []
 $scope.updateScore = function(score){
   $scope.total = mainService.totalScore(score);
+}
+/////////////////////////////////
+//submit final score for round///
+/////////////////////////////////
+$scope.submitScore = function(score){
+   $scope.total=mainService.submitScore(score)
+}
+///////////////////////////////
+///create new player profile///
+///////////////////////////////
+$scope.createPlayer = function(player){
+   $scope.player=mainService.createPlayer(player)
+}
+///////////////////////////////
+///create new course profile///
+///////////////////////////////
+$scope.courseCreate = function(course){
+   console.log(course);
+   mainService.courseCreate(course)
 }
 })
