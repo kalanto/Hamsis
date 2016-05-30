1dc9106c7632042a23c3461b3b6f5fb471e3d57c
kalanto <sandrewbooth@gmail.com>
2016-05-30 17:21:11 -0600

    adding views

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,1 +1,21 @@
-angular.module('app',[])
+angular.module('app',['ui.router'])
+.config(function($stateProvider,$urlRouterProvider,$httpProvider){
+   $stateProvider
+   .state('home',{
+      url:'/'
+   ,  templateUrl:'views/home.html'
+   })
+   .state('course',{
+      url:'/course'
+   ,  templateUrl:'views/course.html'
+   })
+   .state('player',{
+      url:'/player'
+   ,  templateUrl:'views/player.html'
+   })
+   .state('team',{
+      url:'/team'
+   ,  templateUrl:'views/team.html'
+   })
+   $urlRouterProvider.otherwise('/')
+})
